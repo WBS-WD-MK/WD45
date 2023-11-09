6468216283176424923bdc0a963b2a28308c2f73
Mahmoud Khudairi <mahmoudkhudairi@gmail.com>
2023-11-09 11:59:44 +0300

    localstorage

--- week3/booksDemo/index.js
+++ week3/booksDemo/index.js
@@ -1,25 +1,52 @@
 const booksForm = document.querySelector('.books-form');
 const booksList = document.querySelector('.books-list');
-const books = [{ title: 'js is Fun', isRead: true, date: '20-10-2023' }];
+let books = [{ title: 'js is Fun', isRead: true, date: '20-10-2023' }];
 // [{title:"Asdads",isRead:false,date:""},......]
-// #1
+
+// Local Storage
+
+const saveToLocalStorage = () => {
+  // {x,y}
+  // {x:3,y:4}
+  // {x;5}
+  const stringifiedBooks = JSON.stringify(books);
+  localStorage.setItem('books', stringifiedBooks);
+};
+//#1
+const loadFromLocalStorage = () => {
+  const parsedData = JSON.parse(localStorage.getItem('books'));
+  console.log('🚀 ~ file: index.js:64 ~ data:', parsedData);
+
+  // books = parsedData ? parsedData : [];
+  if (parsedData) {
+    books = parsedData;
+  } else {
+    books = [];
+  }
+  // # 2
+  render();
+};
+
 const addBook = event => {
   event.preventDefault();
   const title = event.target.title.value;
   const date = event.target.date.value;
   if (title && date) {
     books.push({ title: title, isRead: false, date: date });
+    saveToLocalStorage();
     render();
   } else {
     alert('Title and date are needed!!!');
   }
+
   event.target.reset();
 };
 
-//#2
+
 const bookButtonHandler = () => {
   console.log('I was clicked!!!!');
 };
+//#3
 const createBookEle = book => {
   // book => {title:"Asdads",isRead:false,date:""}
   const li = document.createElement('li');
@@ -36,14 +63,19 @@ const createBookEle = book => {
   li.append(button);
   return li;
 };
+// #2
 const render = () => {
   booksList.innerHTML = '';
   books.forEach(book => {
     console.log('BOOK??', book);
+    // #3
     const listItem = createBookEle(book);
     booksList.append(listItem);
   });
 };
-
+// #0
 booksForm.addEventListener('submit', addBook);
-render();
+//render()
+
+// #1
+loadFromLocalStorage();
